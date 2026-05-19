fecb82ba5e5e4f5dbf52c5bf78cf133d867cc772
Salma Shah <salma.shah.0516@gmail.com>
2026-05-19 17:13:20 +0530

    Created landing page

---
+++ lib/styles/app_colours.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppColours
+{
+  // brand colours
+  static const primary = Color(0xFF224190);
+  static const secondary = Color(0xFFD39144);
+  static const accent = Color(0xFFFFCD57);
+  static const fill = Color(0xFF8C9BC4);
+
+  // light theme
+  static const lightText = Color(0xFF33363F);
+  static const lightBackground = Color(0xFFFCF5ED);
+  static const lightButtonText = Color(0xFFFFFFFF);
+
+  // dark theme
+
+  
+  // custom colours
+  static const danger = Color(0xFFD20004);
+
+}
---
+++ lib/styles/app_theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'app_colours.dart';
+
+class AppTheme
+{
+  // light theme for now, can add dark theme later
+  static ThemeData lightTheme = ThemeData(
+   brightness: Brightness.light,
+   colorScheme: ColorScheme.light(
+   primary: AppColours.primary, 
+   secondary: AppColours.secondary
+  ),
+
+  scaffoldBackgroundColor: AppColours.lightBackground,
+
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: AppColours.primary,
+      foregroundColor: AppColours.lightButtonText,
+      textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+    ),
+  ),
+  
+  );
+
+  // Define dark theme if needed
+}
