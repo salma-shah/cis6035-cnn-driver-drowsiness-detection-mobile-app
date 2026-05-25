536c5e040d2887c523508523df0d1b6352b20a35
Salma Shah <salma.shah.0516@gmail.com>
2026-05-25 16:20:59 +0530

    Updated custom widgets UI

---
+++ lib/auth/custom_widgets/text_field.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:sleepy_driver/styles/app_colours.dart';
+
+class CustomAuthTextField extends StatelessWidget {
+
+  final String hintText;
+  final IconData prefixIcon;
+  final bool obscureText;
+  final TextEditingController? controller;
+  final AutovalidateMode autovalidateMode;
+  final FormFieldValidator validator;
+
+  const CustomAuthTextField({
+    super.key,
+    required this.hintText,
+    required this.prefixIcon,
+    this.obscureText = false,
+    this.controller,
+    required this.autovalidateMode,
+    required this.validator
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 240,
+      child: TextFormField(
+        style: const TextStyle(
+          fontSize: 14,
+        ),
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          hintText: hintText,
+      
+          prefixIcon: Icon(prefixIcon),
+      
+          filled: true,
+          fillColor: AppColours.lightBackground,
+      
+          contentPadding: const EdgeInsets.symmetric(
+            vertical: 15,
+          ),
+      
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: const BorderSide(
+              color: AppColours.lightText,
+              width: 1.5,
+            ),
+          ),
+      
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide(
+              color: AppColours.fill,
+              width: 2,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/auth/custom_widgets/toast.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:sleepy_driver/styles/app_colours.dart';
+
+class CustomToast {
+  static void show(
+{
+  required BuildContext context, 
+  required String message,
+  required IconData icon,
+  Color bgColor = AppColours.primary,
+  Color txtColor = AppColours.lightButtonText
+}
+) 
+{
+   final overlay =
+        Overlay.of(context);
+    late OverlayEntry overlayEntry;
+    overlayEntry = OverlayEntry(
+      builder: (context) {
+        return Positioned(
+          bottom: 60,
+          left: 10,
+          right: 10,
+
+          child: Material(
+            color: Colors.transparent,
+            child: AnimatedContainer(
+              duration:
+                  const Duration(
+                milliseconds: 3000,
+              ),
+
+              padding:
+                const EdgeInsets.symmetric(
+                horizontal: 14,
+                vertical: 10,
+              ),
+
+              decoration: BoxDecoration(
+                color: bgColor,
+                borderRadius:
+                    BorderRadius.circular(
+                  16,
+                ),
+
+                boxShadow: [
+
+                  BoxShadow(
+                    color: Colors.black
+                        .withValues(alpha: 0.2),
+                    blurRadius: 12,
+                    offset:
+                        const Offset(0, 4),
+                  ),
+                ],
+              ),
+
+              child: Row(
+
+                children: [
+
+                  Icon(
+                    icon,
+                    color: txtColor,
+                  ),
+
+                  const SizedBox(
+                    width: 12,
+                  ),
+
+                  Expanded(
+                    child: Text(
+                      textAlign: TextAlign.center,
+                      message,
+                      style: TextStyle(
+                        color: txtColor,
+                        fontSize: 15,
+                        fontWeight:
+                        FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+
+    overlay.insert(
+      overlayEntry,
+    );
+
+    Future.delayed(
+      const Duration(
+        seconds: 3,
+      ),
+      () {
+        overlayEntry.remove();
+      },
+    );
+}
+}
