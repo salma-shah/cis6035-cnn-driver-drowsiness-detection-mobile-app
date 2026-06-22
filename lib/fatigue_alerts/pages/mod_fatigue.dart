f32a34947e17125781f40b90f3653b4c160df382
Salma Shah <salma.shah.0516@gmail.com>
2026-06-22 14:53:58 +0530

    Package name changed

--- lib/fatigue_alerts/pages/mod_fatigue.dart
+++ lib/fatigue_alerts/pages/mod_fatigue.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:sleepy_driver/shared_custom_widgets/button.dart';
+import 'package:sleepy_driver/shared/custom_widgets/button.dart';
 
 class ModFatigue extends StatelessWidget
 {
