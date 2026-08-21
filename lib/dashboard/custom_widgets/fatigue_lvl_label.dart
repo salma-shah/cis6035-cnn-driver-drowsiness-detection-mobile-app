86c7d179db92718485b24dba00d37ded0af688b2
Salma Shah <salma.shah.0516@gmail.com>
2026-08-21 12:02:13 +0530

    Changed fatigue label layout

--- lib/dashboard/custom_widgets/fatigue_lvl_label.dart
+++ lib/dashboard/custom_widgets/fatigue_lvl_label.dart
@@ -1,43 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:sleepy_driver/dashboard/fatigue_severity.dart';
 
-class FatigueLevelLabel extends StatelessWidget 
-{
+class FatigueLevelLabel extends StatelessWidget {
   final FatigueSeverity fatigueSeverity;
-  const FatigueLevelLabel({super.key, required this.fatigueSeverity});
+  final double? probability;
+
+  const FatigueLevelLabel({
+    super.key,
+    required this.fatigueSeverity,
+    this.probability,
+  });
 
   @override
   Widget build(BuildContext context) {
-    Color lblBgColor, txtColor, borderColor;
+    Color lblBgColor;
+    Color txtColor;
+    Color borderColor;
     String text;
 
-// what to display based on severity
-switch (fatigueSeverity) {
-  case FatigueSeverity.normal:
-    lblBgColor = Color(0xFF74DB8E);
-    txtColor = Color(0xFF00FF40); 
-    borderColor = Color(0xFF00FF40);
-    text = "Active";
-    break;
-    case FatigueSeverity.mild:
-    lblBgColor = Color(0xFFFCFF48);
-    txtColor = Color(0xFFFBFF00); 
-    borderColor = Color(0xFFFBFF00);
-    text = "Drowsy";
-    break;
-    case FatigueSeverity.moderate:
-    lblBgColor = Color(0xFFF68E34);
-    txtColor = Color(0xFFFF5E00); 
-    borderColor = Color(0xFFFF5E00);
-    text = "Moderate";
-    break;
-    case FatigueSeverity.severe:
-    lblBgColor = Color(0xFFFF2A2E);
-    txtColor = Color(0xFFFF0004);; 
-    borderColor = Color(0xFFFF0004);
-    text = "Extreme";
-    break;
-}
+    switch (fatigueSeverity) {
+      case FatigueSeverity.normal:
+        lblBgColor = const Color(0xFF74DB8E);
+        txtColor = const Color(0xFF00FF40);
+        borderColor = const Color(0xFF00FF40);
+        text = 'Active';
+        break;
+
+      case FatigueSeverity.mild:
+        lblBgColor = const Color(0xFFFCFF48);
+        txtColor = const Color(0xFFFBFF00);
+        borderColor = const Color(0xFFFBFF00);
+        text = 'Mild';
+        break;
+
+      case FatigueSeverity.moderate:
+        lblBgColor = const Color(0xFFF68E34);
+        txtColor = const Color(0xFFFF5E00);
+        borderColor = const Color(0xFFFF5E00);
+        text = 'Moderate';
+        break;
+
+      case FatigueSeverity.severe:
+        lblBgColor = const Color(0xFFFF2A2E);
+        txtColor = Color(0xFFFF0004);
+        borderColor = const Color(0xFFFF0004);
+        text = 'Extreme';
+        break;
+    }
+
+    final probabilityText = probability != null
+        ? ' ${(probability! * 100).toStringAsFixed(0)}%'
+        : '';
 
     return Container(
       padding: const EdgeInsets.symmetric(
@@ -48,16 +62,15 @@ switch (fatigueSeverity) {
         color: lblBgColor.withValues(alpha: 0.2),
         borderRadius: BorderRadius.circular(20),
         border: Border.all(
-          color: borderColor.withValues(alpha:0.5),
+          color: borderColor.withValues(alpha: 0.5),
           width: 1.5,
         ),
       ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-        const SizedBox(width: 8),
           Text(
-            text,
+            '$text$probabilityText',
             style: TextStyle(
               color: txtColor,
               fontWeight: FontWeight.w600,
@@ -67,4 +80,5 @@ switch (fatigueSeverity) {
         ],
       ),
     );
-  }}
+  }
+}
