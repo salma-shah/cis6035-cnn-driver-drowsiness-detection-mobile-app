8e1f4236517b0d38e5e8659d49c0067a8f38a220
Salma Shah <salma.shah.0516@gmail.com>
2026-08-26 17:38:27 +0530

    Added abstract interface to user profile service

--- lib/user_profile/repos/user_profile_repo.dart
+++ lib/user_profile/repos/user_profile_repo.dart
@@ -1,17 +1,22 @@
 import 'package:sleepy_driver/auth/models/user.dart';
 import 'package:sleepy_driver/user_profile/services/user_profile_service.dart';
+import 'package:sleepy_driver/user_profile/services/user_profile_service_impl.dart';
 
-class UserProfileRepo
-{
-   final UserProfileService authService = UserProfileService();
+class UserProfileRepo {
+  final UserProfileServiceInterface userProfileService;
 
-   Future<UserModel?> getUserProfile() async
-   {
-    return await authService.getCurrentUser();
-   }
+  UserProfileRepo({
+    UserProfileServiceInterface? userProfileService,
+  }) : userProfileService =
+            userProfileService ?? UserProfileServiceImpl();
 
-   // update user details
-   Future<void> updateUserFields(Map<String,dynamic> fields) async
-    {
-    return await authService.updateUserFields(fields);   }
+  Future<UserModel?> getUserProfile() async {
+    return userProfileService.getCurrentUser();
+  }
+
+  Future<void> updateUserFields(
+    Map<String, dynamic> fields,
+  ) async {
+    return userProfileService.updateUserFields(fields);
+  }
 }
--- lib/user_profile/services/user_profile_service.dart
+++ lib/user_profile/services/user_profile_service.dart
@@ -1,44 +1,12 @@
-import 'dart:async';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:sleepy_driver/auth/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:sleepy_driver/auth/models/user.dart';
 
-class UserProfileService {
-  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-  User? get currentUser => firebaseAuth.currentUser;
+abstract class UserProfileServiceInterface {
+  User? get currentUser;
 
-  // getting current user
- Future<UserModel?> getCurrentUser() async {
-    final firebaseUser = firebaseAuth.currentUser;
+  Future<UserModel?> getCurrentUser();
 
-    if (firebaseUser == null) {
-      return null;
-    }
-
-    final doc = await FirebaseFirestore.instance
-        .collection('users')
-        .doc(firebaseUser.uid)
-        .get();
-
-    if (!doc.exists) {
-      return null;
-    }
-
-    return UserModel.fromMap(doc.data()!);
-  }
-
-    // update name 
-  Future<void> updateUserFields(Map<String, dynamic> fields) async {
-    final currentUser = firebaseAuth.currentUser;
-    if (currentUser == null)
-    {
-      return null;
-    }
-
-    await FirebaseFirestore.instance
-    .collection('users')
-    .doc(currentUser.uid)
-    .update(fields);
-  }
-
+  Future<void> updateUserFields(
+    Map<String, dynamic> fields,
+  );
 }
